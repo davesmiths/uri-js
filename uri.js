37e409d05bdf117593333a65e0f129b9bf71c8a6
Dave Smith <da__smith@hotmail.com>
2011-09-23 16:06:06 +0000

    URL-spaced the app to avoid namespace conflicts. Optimised code. Moved test into tests folder

--- uri.js
+++ uri.js
@@ -1,135 +1,158 @@
-// URI.js  17
+// URI.js  18
 // Creates a URI object with functions URI.parse and URI.stringify
 // http://github.com/davesmith/
 // By Dave Smith: http://www.dave-smith.info/
 
 // Use URI if it exists or create the URI object.
-var URI = URI || {};
+window['github.com/davesmith/uri-js'] = {
 
-// Use URI.parse if it exists or create URI.parse.
-URI.parse = URI.parse || function(uri, undefined) {
-    
-    // Create o, the object to return.
-    var o = {readonly: {source: uri, params: []}, params: {}}, // Return the source URI.
-        colon = ':', // Save a few chars by defining colon.
-        slash = '/', // Save a few chars by defining slash.
-        a = uri.indexOf(colon), // a = index of the first colon to help get the scheme. a is reused later as a temporary store.
-        b = uri.indexOf(slash), // b = index of the first slash to help get the scheme.
-        c = uri.indexOf('?'), // c = index of the first question mark to help get the scheme.
-        d = uri.indexOf('#'); // d = index of the first hash mark.
-    
-    // Remove white space.
-    uri = uri.replace(/^\s+|\s+$/g, '');
-    
-    // Get the scheme:
-    // 1. Checks if there is a colon present.
-    // 2. If present, checks if a slash is absent or if a slash is present whether the colon appears before the first slash.
-    // 3. and checks if a question mark is absent or if a question mark is present whether the colon appears before the first question mark.
-    // 3. Split URI at colons.
-    // 4. Scheme is string before the first colon.
-    // 5. URI is string after the first colon.
-    if (a !== -1 && (b === -1 || a < b) && (c === -1 || a < c)) {
-        uri = uri.split(colon); // uri = [scheme, rest of URI, ...]
-        o.scheme = uri.shift(); // scheme = scheme, uri = [rest of URI, ...]
-        uri = uri.join(colon); // uri = rest of URI
-    }
-    
-    if (o.scheme === undefined || 'http,https,ftp'.split(o.scheme, 2)[1]) {
+    // Use URI.parse if it exists or create URI.parse.
+    parse: function(uri, undefined) {
         
-        // Get the hash:
-        // 1. Split URI at #s.
-        // 2. URI is string before the first #.
-        // 3. Hash is string after the first #.
-        if (d !== -1) {
-            a = uri.split('#'); // a = [rest of URI, hash, ...]
-            uri = a.shift(); // uri = [rest of URI], a = [hash, ...]
-            o.hash = a.join('#') || '';
-        }
+        // Create o, the object to return.
+        var source = uri,
+            readOnlyParams = [],
+            params = {},
+            hash,
+            scheme,
+            user,
+            pass,
+            host,
+            port,
+            path,
+            colon = ':', // Save a few chars by defining colon.
+            slash = '/', // Save a few chars by defining slash.
+            indexOf = 'indexOf', // Saves a few chars when minifying yet the code remains readable.
+            split = 'split',
+            shift = 'shift',
+            join = 'join',
+            a = uri[indexOf](colon), // a = index of the first colon to help get the scheme. a is reused later as a temporary store.
+            b = uri[indexOf](slash), // b = index of the first slash to help get the scheme.
+            c = uri[indexOf]('?'), // c = index of the first question mark to help get the scheme.
+            d = uri[indexOf]('#'); // d = index of the first hash mark.
         
-        // Get the parameters:
-        // 1. Split URI at ?s and only if before the first hash if there is one.
-        // 2. URI is string before first ?.
-        // 3. If there is a string after ?. Split at & and then = to map a params array.
-        if (c !== -1 && (d === -1 || c < d)) {
-            a = uri.split('?'); // a = [rest of URI, query, ...]
-            uri = a.shift(); // uri = rest of URI, a = [query, ...]
-            a = a.join('?').split('&');
-            o.readonly.params = a.map(function(v, k) {
-                v = v.split('=');
-                k = v.shift().replace(/^amp;/, '');
-                v = v.join('=');
-                o.params[k] = v;
-                return {key: k, value: v};
-            });
+        // Remove white space.
+        uri = uri.replace(/^\s+|\s+$/g, '');
+        
+        // Get the scheme:
+        // 1. Checks if there is a colon present.
+        // 2. If present, checks if a slash is absent or if a slash is present whether the colon appears before the first slash.
+        // 3. and checks if a question mark is absent or if a question mark is present whether the colon appears before the first question mark.
+        // 3. Split URI at colons.
+        // 4. Scheme is string before the first colon.
+        // 5. URI is string after the first colon.
+        if (a !== -1 && (b === -1 || a < b) && (c === -1 || a < c)) {
+            uri = uri[split](colon); // uri = [scheme, rest of URI, ...]
+            scheme = uri[shift](); // scheme = scheme, uri = [rest of URI, ...]
+            uri = uri[join](colon); // uri = rest of URI
         }
         
-        // Check if URI is protocol relative.
-        if (uri.substr(0, 2) === slash + slash) {
+        if (scheme === undefined || 'http,https,ftp'[split](scheme, 2)[1]) {
             
-            // Remove the two slashes and split at slashes.
-            uri = (uri.substr(2)).split(slash); // uri = [user:pass@www.asdad.com:8080, segment, ...]
+            // Get the hash:
+            // 1. Split URI at #s.
+            // 2. URI is string before the first #.
+            // 3. Hash is string after the first #.
+            if (d !== -1) {
+                a = uri[split]('#'); // a = [rest of URI, hash, ...]
+                uri = a[shift](); // uri = [rest of URI], a = [hash, ...]
+                hash = a[join]('#') || '';
+            }
             
-            // Take the first item and split at @.
-            a = uri.shift().split('@'); // a = [user:pass, www.asdad.com:8080], uri = [segment, ...]
+            // Get the parameters:
+            // 1. Split URI at ?s and only if before the first hash if there is one.
+            // 2. URI is string before first ?.
+            // 3. If there is a string after ?. Split at & and then = to map a params array.
+            if (c !== -1 && (d === -1 || c < d)) {
+                a = uri[split]('?'); // a = [rest of URI, query, ...]
+                uri = a[shift](); // uri = rest of URI, a = [query, ...]
+                a = a[join]('?')[split]('&');
+                readOnlyParams = a.map(function(v, k) {
+                    v = v[split]('=');
+                    k = v[shift]().replace(/^amp;/, '');
+                    v = v[join]('=');
+                    params[k] = v;
+                    return {key: k, value: v};
+                });
+            }
             
-            // Take the last item and split at colon.
-            b = a.pop().split(colon); // b = [www.asdad.com, 8080], a = [user:pass]
-            a = a.join('@').split(colon); // a = [user, pass]
-            
-            // Get User.
-            o.user = a.shift() || undefined; // user = user, a = [pass]
-            
-            // Get Pass.
-            o.pass = a.join(colon) || undefined; // pass = a = pass
-            
-            // Get Host.
-            o.host = b.shift(); // host = www.asdad.com, b = [8080]
-            
-            // Get Port.
-            o.port = b.join(colon) || undefined; // port = b = 8080
-            
-            // Make URI a string again.
-            uri = slash + uri.join(slash); // uri = path
-        }
-    }
-    
-    // Get Path.
-    o.path = uri || undefined;
-    
-    // Return Object.
-    return o;
-};
-
-// Use URI.stringify if it exists or create URI.stringify.
-URI.stringify = URI.stringify || function(o, undefined) {
-    var uri = (o.scheme) ? o.scheme + ':' : '', params = '';
-    if (o.host) {
-        uri += '//';
-    }
-    if (o.user) {
-        uri +=  o.user;
-        if (o.pass) {
-            uri += ':' + o.pass;
-        }
-        uri += '@';
-    }
-    uri += o.host || '';
-    if (o.port) {
-        uri += ':' + o.port;
-    }
-    uri += o.path || '';
-    if (o.params) {
-        for (var i in o.params) {
-            if (o.params.hasOwnProperty(i)) {
-                params += i + ((o.params[i]) ? '=' + o.params[i] : '') + '&';
+            // Check if URI is protocol relative.
+            if (uri.substr(0, 2) === slash + slash) {
+                
+                // Remove the two slashes and split at slashes.
+                uri = (uri.substr(2))[split](slash); // uri = [user:pass@www.asdad.com:8080, segment, ...]
+                
+                // Take the first item and split at @.
+                a = uri[shift]()[split]('@'); // a = [user:pass, www.asdad.com:8080], uri = [segment, ...]
+                
+                // Take the last item and split at colon.
+                b = a.pop()[split](colon); // b = [www.asdad.com, 8080], a = [user:pass]
+                a = a[join]('@')[split](colon); // a = [user, pass]
+                
+                // Get User.
+                user = a[shift]() || undefined; // user = user, a = [pass]
+                
+                // Get Pass.
+                pass = a[join](colon) || undefined; // pass = a = pass
+                
+                // Get Host.
+                host = b[shift](); // host = www.asdad.com, b = [8080]
+                
+                // Get Port.
+                port = b[join](colon) || undefined; // port = b = 8080
+                
+                // Make URI a string again.
+                uri = slash + uri[join](slash); // uri = path
             }
         }
-        if (params) {
-            uri += '?' + params.slice(0, -1);
+        
+        // Get Path.
+        path = uri || undefined;
+        
+        // Return Object.
+        return {readonly: {source: source, params: readOnlyParams}, params: params, hash: hash, scheme: scheme, user: user, pass: pass, host: host, port: port, path: path};
+    },
+    
+    // Use URI.stringify if it exists or create URI.stringify.
+    stringify: function(o, undefined) {
+        var scheme = o.scheme,
+            paramsB = '',
+            params = o.params,
+            host = o.host,
+            user = o.user,
+            pass = o.pass,
+            port = o.port,
+            hash = o.hash,
+            uri = (scheme) ? scheme + ':' : '',
+            i;
+        if (host) {
+            uri += '//';
         }
+        if (user) {
+            uri +=  user;
+            if (pass) {
+                uri += ':' + pass;
+            }
+            uri += '@';
+        }
+        uri += host || '';
+        if (port) {
+            uri += ':' + port;
+        }
+        uri += o.path || '';
+        if (params) {
+            for (i in params) {
+                if (params.hasOwnProperty(i)) {
+                    paramsB += i + ((params[i]) ? '=' + params[i] : '') + '&';
+                }
+            }
+            if (paramsB) {
+                uri += '?' + paramsB.slice(0, -1);
+            }
+        }
+        if (hash !== undefined) {
+            uri += '#' + hash;
+        }
+        return uri;
     }
-    if (o.hash !== undefined) {
-        uri += '#' + o.hash;
-    }
-    return uri;
 };
